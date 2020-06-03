c827858c8e650a0c60ecb8feaf089a1b254225a8
Romario Gustave <romagustave10@gmail.com>
2020-06-03 18:46:35 +0000

    newlynew

--- lib/bilboard/cli.rb
+++ lib/bilboard/cli.rb
@@ -24,13 +24,16 @@ class Bilboard::CLI
     while input!= "bye"
     input=gets.strip.downcase
     
-    if input.to_i > 0
+    if input.to_i > 0 && input.to_i<6
       the_top5 = @top5[input.to_i-1]
       puts "#{@top5.title}- #{@top5.artist} - #{@top5.rank}- #{@top5.url}"
       
       elsif input == "top"
+      puts "Here is the top 5 again" 
       list_bilboard
-    else
+      elsif input == "bye"
+        bye
+    elsif
         
      case input 
       when "1"
@@ -52,6 +55,6 @@ class Bilboard::CLI
   end 
 end
   def bye 
-  puts "thanks for stopping by see you next time for a new top 5."
+  puts "Thanks for stopping by see you next time for a new top 5."
  end 
  end 
